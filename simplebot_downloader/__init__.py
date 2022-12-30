9a09bd7cdc48d6d6e8c5ce27e9bcb7caba44f7f6
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-12-29 20:03:58 -0500

    Update __init__.py

--- simplebot_downloader/__init__.py
+++ simplebot_downloader/__init__.py
@@ -11,8 +11,8 @@ from simplebot.bot import DeltaBot, Replies
 from .util import FileTooBig, download_file, get_setting, split_download
 
 DEF_MAX_SIZE = str(1024**2 * 300)
-DEF_PART_SIZE = str(1024**2 * 15)
-DEF_DELAY = "60"
+DEF_PART_SIZE = str(1024**2 * 5)
+DEF_DELAY = "5"
 MAX_QUEUE_SIZE = 50
 downloads: Dict[str, Generator] = {}
 
